fe15236e6af55ce59dfc7e00a6c2e11d6b603e88
UENO, M. <eunosium-1128@live.jp>
2026-04-22 15:26:17 +0900

    Update build.bat to modify build configuration
    Disabled ARM support and slim binary option in build script.

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,11 +4,11 @@ setlocal enabledelayedexpansion
 cmake -S . -B build -G "NMake Makefiles JOM" ^
   %CMAKE_ARGS% ^
   -DFILE_CHECK_EXECUTABLE=%PREFIX%/libexec/llvm/FileCheck ^
-  -DARM_ENABLED=OFF ^
   -DISPC_NO_DUMPS=ON ^
-  -DISPC_SLIM_BINARY=ON ^
+  -DISPC_SLIM_BINARY=OFF ^
   -DISPC_INCLUDE_TESTS=ON ^
   -DISPC_INCLUDE_EXAMPLES=OFF ^
-  -DISPC_INCLUDE_RT=OFF
+  -DISPC_INCLUDE_RT=OFF ^
+  -DISPC_INCLUDE_BENCHMARKS=OFF
 cmake --build build --parallel %CPU_COUNT%
 cmake --install build
