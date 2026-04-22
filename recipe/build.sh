0950d47da3e2a196713f25fac8b60ccff352ccf5
UENO, M. <eunosium-1128@live.jp>
2026-04-22 13:03:24 +0900

    Set target architecture based on platform
    Add target architecture variable for CMake build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -exo pipefail
 
+if [[ "${target_platform}" == *"-64" ]]; then
+  target_arch="X86"
+elif [[ "${target_platform}" == *"-aarch64" || "${target_platform}" == *"-arm64" ]]; then
+  target_arch="ARM"
+elif [[ "${target_platform}" == *"-ppc64le" ]]; then
+  target_arch="GENERIC"
+fi
+
 cmake -S . -B build \
   ${CMAKE_ARGS} \
   -DFILE_CHECK_EXECUTABLE=${BUILD_PREFIX}/libexec/llvm/FileCheck \
@@ -9,7 +17,8 @@ cmake -S . -B build \
   -DISPC_SLIM_BINARY=ON \
   -DISPC_INCLUDE_TESTS=ON \
   -DISPC_INCLUDE_EXAMPLES=OFF \
-  -DISPC_INCLUDE_RT=OFF
+  -DISPC_INCLUDE_RT=OFF \
+  -DTARGETS_TO_BUILD="${target_arch}"
 cmake --build build --parallel ${CPU_COUNT}
 cmake --install build
 
