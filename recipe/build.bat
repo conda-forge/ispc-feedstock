51e4f2b712dca698d349a1e8f1543a374dabc5d3
UENO, M. <eunosium-1128@live.jp>
2026-04-22 15:24:33 +0900

    Fix FILE_CHECK_EXECUTABLE path in build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 cmake -S . -B build -G "NMake Makefiles JOM" ^
   %CMAKE_ARGS% ^
-  -DFILE_CHECK_EXECUTABLE=%BUILD_PREFIX%/libexec/llvm/FileCheck ^
+  -DFILE_CHECK_EXECUTABLE=%PREFIX%/libexec/llvm/FileCheck ^
   -DARM_ENABLED=OFF ^
   -DISPC_NO_DUMPS=ON ^
   -DISPC_SLIM_BINARY=ON ^
