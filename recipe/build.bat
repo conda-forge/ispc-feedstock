0010accdf3c5b2f97492d2fa8193f2530738ca82
UENO, M. <eunosium-1128@live.jp>
2026-04-22 20:30:28 +0900

    Change cmake build command to use --verbose flag

--- recipe/build.bat
+++ recipe/build.bat
@@ -28,7 +28,7 @@ cmake -S . -B build -G "NMake Makefiles JOM" ^
     %EXTRA_CMAKE_ARGS%
 if %ERRORLEVEL% neq 0 exit /b 1
 
-cmake --build build --parallel %CPU_COUNT% VERBOSE=1
+cmake --build build --parallel %CPU_COUNT% --verbose
 
 echo === DLL dependency check ===
 dumpbin /dependents %SRC_DIR%\build\bin\ispc.exe
