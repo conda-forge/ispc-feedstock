97fbae24601884e12b79c037f0c1e666a2601e9c
UENO, M. <eunosium-1128@live.jp>
2026-04-23 12:05:20 +0900

    Fix path in build.bat for Release directory

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,7 @@
 @echo on
 setlocal enabledelayedexpansion
 
-set PATH=%SRC_DIR%\build\bin;%SRC_DIR%\build\Release\bin;%BUILD_PREFIX%\Library\bin;%BUILD_PREFIX%\bin;%PREFIX%\Library\bin;%PREFIX%\bin;%PATH%
+set PATH=%SRC_DIR%\build\bin;%SRC_DIR%\build\bin\Release\;%BUILD_PREFIX%\Library\bin;%BUILD_PREFIX%\bin;%PREFIX%\Library\bin;%PREFIX%\bin;%PATH%
 if %ERRORLEVEL% neq 0 exit /b 1
 
 set EXTRA_CMAKE_ARGS=
